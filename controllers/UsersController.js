c11543a2f4d2f03ead2fc8ef3771ef571cacc6ab
Nazzcodek <bellnas09@gmail.com>
2024-05-11 02:14:29 -0700

    lets see again

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,7 +4,6 @@ const dbClient = require('../utils/db');
 
 const postNew = async (req, res) => {
   const { email, password } = req.body;
-  const dbClient = await dbClient();
 
   if (!email) {
     return res.status(400).json({ error: 'Missing email' });
--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Server running on por ${port}`);
+  console.log(`Server running on port ${port}`);
 });
