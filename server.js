992818cf5c9cd58d513dd8a09a63453742a16296
Nazzcodek <bellnas09@gmail.com>
2024-05-11 00:24:59 -0700

    server lint

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ import express from 'express';
 
 const app = express();
 const port = 5000 || process.env.PORT;
-const routes = require('./routes/index.js');
+
+const routes = require('./routes/index');
+
+app.use(express.json());
+app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
